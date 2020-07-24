f74e3c9b28b6e98476904d609706068165d0ac09
pastukhVitalii <pastukh.v.7@gmail.com>
2020-07-24 20:40:03 +0300

    Updates

---
+++ precache-manifest.c93e148e59323bafe76f72ee878625e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dee54ee0f5d0c0a81fe290a500b3c10",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "df0613c7ae5506e00cfd",
+    "url": "/cards/static/css/main.50733283.chunk.css"
+  },
+  {
+    "revision": "506c0c2f9e777253f6da",
+    "url": "/cards/static/js/2.770cdc58.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards/static/js/2.770cdc58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df0613c7ae5506e00cfd",
+    "url": "/cards/static/js/main.a2fcef4a.chunk.js"
+  },
+  {
+    "revision": "8e9c10436e990c8ebaef",
+    "url": "/cards/static/js/runtime-main.3640371d.js"
+  }
+]);
