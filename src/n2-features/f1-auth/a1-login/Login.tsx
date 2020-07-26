fd0aa79c2a228093904586a2bc06ac9170f4b515
rdnk84 <rdnk_e@mail.ru>
2020-07-26 23:11:24 +0300

    For login page: jsx added, useState and useCallBacks added

--- src/n2-features/f1-auth/a1-login/Login.tsx
+++ src/n2-features/f1-auth/a1-login/Login.tsx
@@ -1,23 +1,50 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 import Input from "../../../n1-main/m1-ui/common/input/Input";
 import Button from "../../../n1-main/m1-ui/common/button/Button";
 
 const Login = (props: any) => {
 
-    const onClick = () => {
+    const [email, setEmail] = useState();
+    const [pass, setPass] = useState();
+    const [rememberMe, setRememberMe] = useState(false);
+
+
+    const signIn = () => {
         alert('fdgfhf')
     }
-    const onChange = () => {
 
-    }
 
-    let btnStyle ='' || 'primary';
-    let inputStyle ='' || 'error';
+    const setEmailCallBack = useCallback((e) => {
+        setEmail(e.currentTarget.value)
+    }, [setEmail]);
+
+    const setPasswordCallBack = useCallback((e) => {
+        setPass(e.currentTarget.value)
+    }, [setPass]);
+
+
+    let btnStyle = '' || 'primary';
+    let inputStyle = '' || 'error';
     return (
         <>
-            <div >
-                <Input type={inputStyle} placeholder={'placeholder'} value={null} onChange={onChange}/>
-                <Button type={btnStyle} name={'name'}  onClick={onClick} spiner={true}/> {/*primary danger loading*/}
+            <div>
+                <form action="">
+                    <div>
+                        <Input type={inputStyle} placeholder={'e-mail'} value={email} onChange={setEmailCallBack}/>
+                    </div>
+                    <div>
+                        <Input type={inputStyle} placeholder={'password'} value={pass} onChange={setPasswordCallBack}/>
+                    </div>
+                    <div>
+                        <label>
+                            <input type={'checkbox'}/> Remember me
+                        </label>
+                    </div>
+                    <div><a href="">Forgot?</a></div>
+                    <Button type={btnStyle} name={'Login'} onClick={signIn}
+                            spiner={false} disable={false}/> {/*primary danger loading*/}
+                </form>
+
             </div>
         </>
     );
